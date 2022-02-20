1c58e6a8992c313f8deea02aeb9660de62ed9f95
Musa <codermusa@gmail.com>
2022-02-20 16:16:39 +0300

    finished implementing infinite slider for left and right buttons

--- public/js/index-1.js
+++ public/js/index-1.js
@@ -12,154 +12,102 @@ const rightControl = document.querySelector(".slider-control--right");
 let sliderState = {
   parent: slider,
   distance: 0,
-  length: slides.length,
-  direction: null,
+  direction: "L",
   leftControl,
   rightControl,
-  left: null,
-  right: null,
-  original: slides,
-  edited: null,
-  clonedLeft: null,
-  clonedRight: null,
-  transition: true,
-  slidesToMove: "edited",
-  lastSlide: slides[slides.length - 1],
-  firstSlide: slides[0],
-  index: 0,
+  transition: false,
   offset: 100,
+  slides: slides,
+  sliderLength: undefined,
+  firstClone: slides[0].cloneNode(true),
+  lastClone: slides[slides.length - 1].cloneNode(true),
 };
 
-//-------------------------------------------------------------------
-// must provide state direction + state transition on call
+//============================================================================
 
-const moveSlides = (state) => {
+sliderState.firstClone.id = "first-clone";
+sliderState.lastClone.id = "last-clone";
+
+slider.append(sliderState.firstClone);
+slider.prepend(sliderState.lastClone);
+
+sliderState.slides = sliderState.parent.querySelectorAll(".slider-item");
+sliderState.sliderLength = sliderState.slides.length - 1;
+
+moveSlides(sliderState);
+sliderState.transition = true;
+
+//============================================================================
+
+function moveSlides(state) {
   state.direction === "R" && state.distance++;
   state.direction === "L" && state.distance--;
+  if (state.distance < -5 || state.distance > 0) return;
+  console.log(state.distance);
+  // let distance = Math.abs(state.distance);
 
-  for (let [i, slide] of state[state.slidesToMove].entries()) {
-    slide.setAttribute("id", i + 1);
-    slide.setAttribute("distance", state.distance * state.offset);
+  if (state.slides[Math.abs(state.distance)].id === state.firstClone.id) {
+    const first = state.firstClone;
 
+    first.addEventListener(
+      "transitionend",
+      () => {
+        console.log(state.firstClone.id);
+        state.distance = 0;
+        state.direction = "L";
+        state.transition = false;
+        moveSlides(state);
+        state.transition = true;
+      },
+      { once: true }
+    );
+  }
+
+  if (state.slides[Math.abs(state.distance)].id === state.lastClone.id) {
+    const last = state.lastClone;
+
+    last.addEventListener(
+      "transitionend",
+      () => {
+        console.log(state.lastClone.id);
+        state.distance = 0;
+        state.direction = "L";
+        state.transition = false;
+        moveSlides(state);
+        state.transition = true;
+      },
+      { once: true }
+    );
+  }
+
+  for (let [i, slide] of state.slides.entries()) {
     const yesTrans = "transform cubic-bezier(1,0,0,1) 1s";
     const noTrans = "none";
+
+    if (!slide["id"]) slide.id = i;
 
     (state.transition && (slide.style.transition = yesTrans)) ||
       (slide.style.transition = noTrans);
 
     slide.style.transform = `translateX(${state.distance * state.offset}%)`;
   }
-};
-
-//-------------------------------------------------------------------
-// must provide position to clone(set cloneLeft/right to true)
-
-const cloneSlide = (state) => {
-  state.original = state.parent.querySelectorAll(".slider-item");
-  const length = state.original.length - 1;
-  const first = state.original[0].cloneNode(true);
-  first.setAttribute("name", "first");
-  const last = state.original[length].cloneNode(true);
-  last.setAttribute("name", "last");
-
-  if (!state.clonedLeft && state.left) {
-    state.clonedLeft = true;
-    state.parent.append(first);
-  }
-
-  if (!state.clonedRight && state.right) {
-    state.clonedRight = true;
-    state.parent.append(first);
-  }
-
-  state.edited = state.parent.querySelectorAll(".slider-item");
-  state.length = state.edited.length;
-  state.lastSlide = state.edited[state.length - 1];
-  state.firstSlide = state.edited[0];
-  return state;
-};
-
-//-------------------------------------------------------------------
-// must set index to be remove in state
-const removeNode = (state) => {
-  state.edited = state.parent.querySelectorAll(".slider-item");
-  state.parent.removeChild(state.edited[state.index]);
-};
+}
 
 //-------------------------------------------------------------------
 // EVENTS
 //-------------------------------------------------------------------
 
 sliderState.leftControl.addEventListener("click", () => {
-  action("L");
+  sliderState.direction = "L";
+  action(sliderState);
 });
-
-//-------------------------------------------------------------------
 
 sliderState.rightControl.addEventListener("click", () => {
-  action("R");
+  sliderState.direction = "R";
+  action(sliderState);
 });
 
-//-------------------------------------------------------------------
-
-// setInterval(() => {
-//   action("L");
-// }, 5000);
-
-//-------------------------------------------------------------------
-
-function action(direction) {
-  console.log(sliderState);
-  sliderState.slidesToMove = "edited";
-  sliderState.transition = true;
-
-  if (direction === "L") {
-    sliderState.direction = "L";
-    sliderState.left = true;
-    sliderState = cloneSlide(sliderState);
-    sliderState.left = null;
-  }
-  if (direction === "R") {
-    sliderState.direction = "R";
-    sliderState.right = true;
-  }
-
-  if (direction === "L") {
-    sliderState.slidesToMove = "original";
-    moveSlides(sliderState);
-
-    if (sliderState.distance === -(sliderState.length - 1)) {
-      sliderState.lastSlide.addEventListener("transitionend", () => {
-        sliderState.index = sliderState.length - 1;
-        removeNode(sliderState);
-        sliderState.clonedLeft = null;
-        sliderState.transition = null;
-        sliderState.slidesToMove = "original";
-        sliderState.distance = 1;
-        moveSlides(sliderState);
-      });
-    } else {
-      sliderState = cloneSlide(sliderState);
-      sliderState.right = null;
-    }
-  }
-  if (direction === "R") {
-    if (sliderState.distance === 0) {
-      sliderState.parent.style.flexDirection = "row-reverse";
-      sliderState = cloneSlide(sliderState);
-    }
-    moveSlides(sliderState);
-
-    if (sliderState.distance === sliderState.length - 1) {
-      sliderState.lastSlide.addEventListener("transitionend", () => {
-        sliderState.index = sliderState.length - 1;
-        removeNode(sliderState);
-        sliderState.clonedRight = null;
-        sliderState.transition = null;
-        sliderState.parent.style.flexDirection = "row";
-        sliderState.distance = -1;
-        moveSlides(sliderState);
-      });
-    }
-  }
+function action(state) {
+  console.log("moving to: ", state.direction);
+  moveSlides(state);
 }
